ab68e3fca3a661bfb8cc518f3d55fec40a85302b
Steevenson Jean <steevej470@gmail.com>
2025-12-25 14:36:49 -0500

    ReservationControllerTest createReservation & getAllReservations test implemented

--- src/test/java/com/personal/chauffeur_reservation_system/controller/ReservationControllerTest.java
+++ src/test/java/com/personal/chauffeur_reservation_system/controller/ReservationControllerTest.java
@@ -2,49 +2,49 @@ package com.personal.chauffeur_reservation_system.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.personal.chauffeur_reservation_system.dto.CustomerDto;
+import com.personal.chauffeur_reservation_system.dto.ReservationDto;
 import com.personal.chauffeur_reservation_system.exceptions.CustomerNotFoundException;
 import com.personal.chauffeur_reservation_system.mapper.CustomerMapper;
+import com.personal.chauffeur_reservation_system.mapper.ReservationMapper;
 import com.personal.chauffeur_reservation_system.model.Customer;
+import com.personal.chauffeur_reservation_system.model.Reservation;
 import com.personal.chauffeur_reservation_system.service.CustomerService;
+import com.personal.chauffeur_reservation_system.service.ReservationService;
 
-import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-@WebMvcTest(controllers = CustomerController.class)
+@WebMvcTest(controllers = ReservationController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
-public class CustomerControllerTest {
+public class ReservationControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean
+    private ReservationService reservationService;
 
     @MockitoBean
     private CustomerService customerService;
@@ -55,10 +55,15 @@ public class CustomerControllerTest {
     private Customer customer;
 
     private CustomerDto customerDto;
+    
+    private Reservation reservation;
+
+    private ReservationDto reservationDto;
 
     @BeforeEach
     public void init() {
         customer = Customer.builder()
+            .id(1L)
             .firstName("Brandon")
             .lastName("Williams")
             .email("brandonWilliams@vsu.edu")
@@ -67,40 +72,50 @@ public class CustomerControllerTest {
             .build();
         customerDto = CustomerMapper.mapToCustomerDto(customer);
 
+        Reservation reservation = Reservation.builder()
+            .id(1L)
+            .pickupAddress("217 Princeton Court")
+            .destination("1400 Townpark Drive")
+            .date(LocalDate.now())
+            .time(LocalTime.now())
+            .customer(customer)
+            .build();
+
+        reservationDto = ReservationMapper.mapReservationToReservationDto(reservation);
+
     }
 
     @Test
-    public void CustomerController_CreateCustomer_ReturnCreatedCustomer() throws Exception {
-        when(customerService.createCustomer(any(CustomerDto.class))).thenReturn(customerDto);
+    public void ReservationController_CreateReservation_ReturnReservationDtoResponse() throws Exception {
+        when(reservationService.createReservation(any(ReservationDto.class))).thenReturn(reservationDto);
 
-        ResultActions response = mockMvc.perform(post("/customers/add-customer")
+        ResultActions response = mockMvc.perform(post("/reservations/add-reservation")
             .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(customerDto)));
-
+            .content(objectMapper.writeValueAsString(reservationDto)));
+            
         response.andExpect(MockMvcResultMatchers.status().isCreated())
-            //.andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(customerDto.getFirstName())))
             .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
-    public void CustomerController_GetAllCustomers_ReturnCustomerDtoResponse() throws Exception {
-        Customer customer2 = Customer.builder()
-            .firstName("William")
-            .lastName("Regal")
-            .email("theArchitect@vsu.edu")
-            .phoneNumber("331-243-2921")
-            .reservations(new ArrayList<>())
+    public void ReservationController_GetAllReservations_ReturnListOfReservationResponse() throws Exception {
+        Reservation reservation2 = Reservation.builder()
+            .id(1L)
+            .pickupAddress("1500 North Patterson Street")
+            .destination("1400 Townpark Drive")
+            .date(LocalDate.now())
+            .time(LocalTime.now())
+            .customer(customer)
             .build();
+        ReservationDto reservationDto2 = ReservationMapper.mapReservationToReservationDto(reservation2);
+        List<ReservationDto> reservations = Arrays.asList(reservationDto, reservationDto2);
 
-        CustomerDto customerDto2 = CustomerMapper.mapToCustomerDto(customer2);
-        List<CustomerDto> customerDtos = Arrays.asList(customerDto, customerDto2);
-        when(customerService.getAllCustomers()).thenReturn(customerDtos);
+        when(reservationService.getReservations()).thenReturn(reservations);
 
-        ResultActions response = mockMvc.perform(get("/customers")
+        ResultActions response = mockMvc.perform(get("/reservations")
             .contentType(MediaType.APPLICATION_JSON));
         
-            response.andExpect(MockMvcResultMatchers.status().isOk());
-                
+        response.andExpect(MockMvcResultMatchers.status().isOk());
     }
-
+    
 }
