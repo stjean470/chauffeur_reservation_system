1612aaaf686c4ca9895268d492953f69ad3a2800
Steevenson Jean <steevej470@gmail.com>
2025-12-25 13:32:04 -0500

    CustomerControllerTest GetAllCustomers test have been implemented

--- src/test/java/com/personal/chauffeur_reservation_system/controller/CustomerControllerTest.java
+++ src/test/java/com/personal/chauffeur_reservation_system/controller/CustomerControllerTest.java
@@ -4,7 +4,11 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,6 +22,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -29,6 +35,7 @@ import com.personal.chauffeur_reservation_system.service.CustomerService;
 
 import static org.mockito.Mockito.when;
 import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -63,14 +70,36 @@ public class CustomerControllerTest {
     }
 
     @Test
-    public void CustomerController_CreateCustomer_ReturnCreatedCustomer() throws CustomerNotFoundException, Exception {
+    public void CustomerController_CreateCustomer_ReturnCreatedCustomer() throws Exception {
         when(customerService.createCustomer(any(CustomerDto.class))).thenReturn(customerDto);
 
         ResultActions response = mockMvc.perform(post("/customers/add-customer")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(customerDto)));
 
-        response.andExpect(MockMvcResultMatchers.status().isCreated());
+        response.andExpect(MockMvcResultMatchers.status().isCreated())
+            //.andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(customerDto.getFirstName())))
+            .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void CustomerController_GetAllCustomers_ReturnCustomerDtoResponse() throws Exception {
+        Customer customer2 = Customer.builder()
+            .firstName("William")
+            .lastName("Regal")
+            .email("theArchitect@vsu.edu")
+            .phoneNumber("331-243-2921")
+            .reservations(new ArrayList<>())
+            .build();
+
+        CustomerDto customerDto2 = CustomerMapper.mapToCustomerDto(customer2);
+        List<CustomerDto> customerDtos = Arrays.asList(customerDto, customerDto2);
+        when(customerService.getAllCustomers()).thenReturn(customerDtos);
+
+        ResultActions response = mockMvc.perform(get("/customers")
+            .contentType(MediaType.APPLICATION_JSON));
+        
+            response.andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
